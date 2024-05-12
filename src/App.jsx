88c45884d380be5730bdd44312308ca5f9447a75
rejina <rejinakhadka00@gmail.com>
2024-05-12 22:02:27 +0545

    feat(UI-DiscoverSection)

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import Herosection from './Components/Hero/Herosection';
-import Company from './Components/Companies/Company';
-import Automated from './Components/Automated/Automated';
-import Cards from './Components/Cards/Cards';
-import Whychooseus from './Components/Whychooseus/Whychooseus';
-import Savemoney from './Components/SaveMoney/Savemoney';
-import Features from './Components/Features/Features';
-import Testimonials from './Components/Testimonials/Testimonials';
-import Reviews from './Components/Reviews/Reviews';
-import { Blogs } from './Components/Blogs/Blogs';
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Herosection from "./Components/Hero/Herosection";
+import Company from "./Components/Companies/Company";
+import Automated from "./Components/Automated/Automated";
+import Cards from "./Components/Cards/Cards";
+import Whychooseus from "./Components/Whychooseus/Whychooseus";
+import Savemoney from "./Components/SaveMoney/Savemoney";
+import Features from "./Components/Features/Features";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Reviews from "./Components/Reviews/Reviews";
+import { Blogs } from "./Components/Blogs/Blogs";
 
-import Frequently from './Components/Frequently/Frequently';
+import Frequently from "./Components/Frequently/Frequently";
+import Discover from "./Components/Discover/Discover";
 
 const App = () => {
   const divStyle = {
-    backgroundImage: 'linear-gradient(to right, #62BADA 0%, #C9E7F2 100%)'
+    backgroundImage: "linear-gradient(to right, #62BADA 0%, #C9E7F2 100%)",
   };
 
   return (
-    <div >
-       <div style={divStyle}>
-      <Navbar />
-      <Herosection />
+    <div>
+      <div style={divStyle}>
+        <Navbar />
+        <Herosection />
+      </div>
+      <Company />
+      <Automated />
+      <Cards />
+      <Whychooseus />
+      <Savemoney />
+      <Features />
+      <Testimonials />
+      <Reviews />
+      <Blogs />
+      <Frequently />
+      <Discover />
     </div>
-    <Company/>
-    <Automated/>
-    <Cards/>
-    <Whychooseus/>
-    <Savemoney/>
-    <Features/>
-    <Testimonials/>
-    <Reviews/>
-    <Blogs/>
- <Frequently/>
-    </div>
-   
-    
   );
 };
 
---
+++ src/Components/Discover/Discover.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+const Discover = () => {
+  return (
+    <div className="discover-container">
+      <div className="discover-content">
+        <h1>Discover a better way to manage spendings</h1>
+        <button>Get Started Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
